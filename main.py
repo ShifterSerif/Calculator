14208a86d27cef98ab8fcebdd7d4f5d4988ac5ce
Erick Sanchez <sanchez8erick@gmail.com>
2023-10-18 16:29:44 -0400

    Update main.py
    Added single line equations

--- main.py
+++ main.py
@@ -1,41 +1,58 @@
 # use space to separate numbers and operators for single line?
 
-def main():
-    print("Enter a number or symbol to add to equation. Enter '=' to solve equation. Enter 'q' to backspace.")
-    i = 0
-    operations = 0
+def onebyone():
+    print("Enter a number or symbol to add to equation. Enter '=' to solve equation. Enter 'b' to backspace.")
+    i = 1
     inputs = []
     while True:
         choice = input()
         if choice == '=':
             break
         elif choice.isdigit():
-            choice = int(choice)
-        elif choice == '+' or choice == '-' or choice == '*' or choice == '/' or choice == '^':
-            operations += 1
-        else:
+            choice = float(choice)
+        elif choice != '+' and choice != '-' and choice != '*' and choice != '/' and choice != '^':
             print("Invalid input")
             continue
         inputs.append(choice)
-        if choice == 'q':
+        if choice == 'b':
             inputs.pop()
             inputs.pop()
     result = inputs[0]
-    while operations > 0:
-        if inputs[i+1] == '+':
-            result += inputs[i+2]
-        elif inputs[i+1] == '-':
-            result -= inputs[i+2]
-        elif inputs[i+1] == '*':
-            result *= inputs[i+2]
-        elif inputs[i+1] == '/':
-            result /= inputs[i+2]
-        elif inputs[i+1] == '^':
-            result **= inputs[i+2]
-        operations -= 1
+    while i < len(inputs):
+        if inputs[i] == '+':
+            result += inputs[i+1]
+        elif inputs[i] == '-':
+            result -= inputs[i+1]
+        elif inputs[i] == '*':
+            result *= inputs[i+1]
+        elif inputs[i] == '/':
+            result /= inputs[i+1]
+        elif inputs[i] == '^':
+            result **= inputs[i+1]
         i += 2
     print(inputs)
     print(result)
 
 
-main()
+def allatonce():
+    print("Enter an equation to solve. Use spaces to separate numbers and operators.")
+    equation = input().split()
+    result = float(equation[0])
+    i = 1
+    while i < len(equation):
+        if equation[i] == '+':
+            result += float(equation[i+1])
+        elif equation[i] == '-':
+            result -= float(equation[i+1])
+        elif equation[i] == '*':
+            result *= float(equation[i+1])
+        elif equation[i] == '/':
+            result /= float(equation[i+1])
+        elif equation[i] == '^':
+            result **= float(equation[i+1])
+        i += 2
+    print(result)
+
+
+# onebyone()
+allatonce()
